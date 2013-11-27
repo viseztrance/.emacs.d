3d5bc83b8df46f60a7a5d158501159aa2b42092f
Daniel Mircea <daniel@viseztrance.com>
2013-11-27 23:51:23 +0200

    Properly load flycheck modifiers

--- config/flycheck-setup.el
+++ config/flycheck-setup.el
@@ -3,9 +3,8 @@
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
 (eval-after-load 'flycheck
-  (progn '(setq flycheck-checkers
-		(delq 'emacs-lisp-checkdoc flycheck-checkers))
-	 '(set-face-background 'flycheck-error "red3")
-	 '(set-face-background 'flycheck-warning "dark slate blue")))
+  '(progn (setq flycheck-checkers
+                (delq 'emacs-lisp-checkdoc flycheck-checkers))
+          (set-face-background 'flycheck-error "red3")))
 
 (provide 'flycheck-setup)
