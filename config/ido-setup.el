079e7fac3df3e493c8562f328ec4dc2131429dc5
Daniel Mircea <daniel@viseztrance.com>
2013-11-27 21:29:02 +0200

    Add vertical ido

--- config/ido-setup.el
+++ config/ido-setup.el
@@ -1,7 +1,11 @@
 ;; Buffer autocompletion
+(require-package 'ido-vertical-mode)
+
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
 (setq ido-ignore-buffers  '("\\` " "^\*Mess" "^\*Back" ".*Completion" "^\*Ido" "^\*trace" "^\*compilation" "^\*GTAGS" "^session\.*"))
+
 (ido-mode 1)
+(ido-vertical-mode 1)
 
 (provide 'ido-setup)
