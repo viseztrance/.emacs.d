9937440cb46db54785de4ceceb124fceb669a125
Daniel Mircea <daniel@viseztrance.com>
2013-11-27 23:52:20 +0200

    Clean file on save and properly display whitespace

--- config/misc-setup.el
+++ config/misc-setup.el
@@ -11,7 +11,24 @@
 
 ;; Show trailing whitespaces
 (setq-default show-trailing-whitespace t)
+(set-face-background 'trailing-whitespace "snow4")
 
+;; Easily toggle whitespace
+(global-set-key "\C-cw" 'whitespace-mode)
+
+;; Use spaces instead of tabs
+(setq-default indent-tabs-mode nil)
+
+;; Untabify file on save and delete trailing whitespace
+(setq tabbed-modes '(go-mode makefile-gmake-mode))
+(defun untabify-buffer ()
+  (interactive)
+  (untabify (point-min) (point-max)))
+(add-hook 'before-save-hook '(lambda ()
+                               (progn
+                                 (delete-trailing-whitespace)
+                                 (unless (member major-mode tabbed-modes)
+                                   (untabify-buffer) ()))))
 ;; Align with spaces only
 (defadvice align-regexp (around align-regexp-with-spaces)
   "Never use tabs for alignment."
