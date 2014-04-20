221c46276ed879c3cbc77520dc32563f2fff2ad3
Daniel Mircea <daniel@viseztrance.com>
2014-04-20 11:35:51 +0300

    Adjust projectile hotkeys

--- config/projectile-setup.el
+++ config/projectile-setup.el
@@ -1,6 +1,8 @@
 (require-package 'projectile)
 
 (projectile-global-mode)
-(global-set-key [f2] 'projectile-find-file)
+
+(global-set-key [f2] 'projectile-switch-project)
+(global-set-key (kbd "C-.") 'projectile-find-file)
 
 (provide 'projectile-setup)
