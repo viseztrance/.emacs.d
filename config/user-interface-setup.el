4ecb641f417e656f59d26b3f122341a9d1d4457b
Daniel Mircea <daniel@viseztrance.com>
2014-04-20 11:50:06 +0300

    Use a custom mode line

--- config/user-interface-setup.el
+++ config/user-interface-setup.el
@@ -1,16 +1,3 @@
-;; Set color theme
-(require-package 'tangotango-theme)
-(load-theme 'tangotango t)
-
-(set-face-attribute 'default nil :height 120)
-(set-face-background 'region "gray30")
-
-(add-hook 'post-command-hook
-          (lambda ()
-            (let ((color
-                   (if (buffer-modified-p) "white" "gray")))
-              (set-face-attribute 'mode-line-buffer-id nil :foreground color))))
-
 ;; Disable menu / toolbar
 (menu-bar-mode 0)
 (tool-bar-mode 0)
@@ -27,5 +14,62 @@
 
 ;; Hide welcome screen
 (setq inhibit-startup-screen t)
+
+;; Set color theme
+(require-package 'tangotango-theme)
+(load-theme 'tangotango t)
+
+;; Set the mode line
+(set-face-attribute 'default nil :height 120)
+(set-face-background 'region "gray30")
+
+(make-face 'mode-line-position-face)
+(make-face 'mode-line-position-large-face)
+(make-face 'mode-line-filename-face)
+(make-face 'mode-line-modified-filename-face)
+(make-face 'mode-line-readonly-filename-face)
+(make-face 'mode-line-major-mode-face)
+(make-face 'mode-line-minor-modes-face)
+
+(set-face-attribute 'mode-line-position-face nil
+                    :foreground "dark orange")
+
+(set-face-attribute 'mode-line-position-large-face nil
+                    :inherit 'mode-line-position-face
+                    :foreground "salmon")
+
+(set-face-attribute 'mode-line-filename-face nil
+                    :foreground "#000"
+                    :background "dark orange")
+
+(set-face-attribute 'mode-line-modified-filename-face nil
+                    :inherit 'mode-line-filename-face
+                    :foreground "#fff")
+
+(set-face-attribute 'mode-line-readonly-filename-face nil
+                    :inherit 'mode-line-filename-face
+                    :foreground "#fff"
+                    :background "darkorange4")
+
+(set-face-attribute 'mode-line-major-mode-face nil
+                    :foreground "#999")
+
+(set-face-attribute 'mode-line-minor-modes-face nil
+                    :foreground "#555")
+
+(setq-default
+ mode-line-format
+ '(
+   (:eval (propertize "  %b  " 'face (cond (buffer-read-only 'mode-line-readonly-filename-face)
+                                       ((buffer-modified-p) 'mode-line-modified-filename-face)
+                                       (t 'mode-line-filename-face))))
+   (:eval (propertize " %3c  " 'face (if  (>= (current-column) 80)
+                                        'mode-line-position-large-face
+                                      'mode-line-position-face)))
+   (:propertize mode-name face mode-line-major-mode-face)
+   (:eval (propertize (format-mode-line minor-mode-alist) 'face 'mode-line-minor-modes-face))
+   (vc-mode vc-mode)))
+
+(set-face-background 'modeline-inactive "#ccc")
 
 (provide 'user-interface-setup)
