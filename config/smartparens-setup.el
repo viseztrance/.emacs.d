cf4c3f3cfc311988ab6917cd3af2ec0ca826f375
Daniel Mircea <daniel@viseztrance.com>
2014-04-21 17:02:02 +0300

    Change smartparens selection face

--- config/smartparens-setup.el
+++ config/smartparens-setup.el
@@ -3,4 +3,7 @@
 (require 'smartparens-config)
 (smartparens-global-mode 1)
 
+(set-face-attribute 'sp-pair-overlay-face nil
+                    :background "gray30")
+
 (provide 'smartparens-setup)
