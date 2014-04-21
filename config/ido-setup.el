77060805181cb8b890aca6c105adb9d6e854f0ff
Daniel Mircea <daniel@viseztrance.com>
2014-04-21 21:31:31 +0300

    Ignore additional buffers and change highlighting

--- config/ido-setup.el
+++ config/ido-setup.el
@@ -6,7 +6,20 @@
 (require 'flx-ido)
 
 (setq ido-everywhere t)
-(setq ido-ignore-buffers  '("\\` " "^\*Mess" "^\*Back" ".*Completion" "^\*Ido" "^\*trace" "^\*compilation" "^\*GTAGS" "^session\.*"))
+(setq ido-ignore-buffers '("\\` "
+                           "^\*Mess"
+                           "^\*Back"
+                           ".*Completion"
+                           "^\*Ido"
+                           "^\*trace"
+                           "^\*compilation"
+                           "^\*GTAGS"
+                           "^\*magit-process"
+                           "^\*Flycheck"
+                           "^session\.*"))
+
+(set-face-attribute 'flx-highlight-face nil
+                    :underline nil)
 
 (ido-mode t)
 (flx-ido-mode t)
