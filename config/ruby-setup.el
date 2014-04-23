66f9b0ecb15e1d682b0b4f9f736bb07015a34580
Daniel Mircea <daniel.mircea@geminisols.ro>
2014-04-23 19:00:08 +0300

    Indent ruby files automatically

--- config/ruby-setup.el
+++ config/ruby-setup.el
@@ -4,4 +4,7 @@
 
 (associate-files 'ruby ".rake" ".thor" "Gemfile" "Rakefile" "Capfile")
 
+;; Indent files automatically
+(add-hook 'ruby-mode-hook (lambda () (local-set-key "\r" 'newline-and-indent)))
+
 (provide 'ruby-setup)
