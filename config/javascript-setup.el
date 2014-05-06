580ce08d31b6da68d56875785c8b396c64ab925d
Daniel Mircea <daniel.mircea@geminisols.ro>
2014-05-06 14:10:00 +0300

    Ignore certain js functions and common libraries

--- config/javascript-setup.el
+++ config/javascript-setup.el
@@ -2,4 +2,15 @@
 
 (associate-files 'js2 ".js" ".js.erb")
 
+(setq-default js2-global-externs '("module"
+                                   "require"
+                                   "jQuery"
+                                   "$"
+                                   "setTimeout"
+                                   "clearTimeout"
+                                   "setInterval"
+                                   "clearInterval"
+                                   "console"
+                                   "JSON"))
+
 (provide 'javascript-setup)
