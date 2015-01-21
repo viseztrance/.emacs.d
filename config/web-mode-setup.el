e859270c058f9e9f4256606cf9d47999df77475f
Daniel Mircea <daniel.mircea@geminisols.com>
2015-01-21 11:53:49 +0200

    Associate web mode with html erb templates

--- config/web-mode-setup.el
+++ config/web-mode-setup.el
@@ -1,6 +1,6 @@
 (require-package 'web-mode)
 
-(associate-files 'web ".php")
+(associate-files 'web ".php" ".html.erb")
 (setq web-mode-code-indent-offset 4)
 (setq web-mode-markup-indent-offset 4)
 
