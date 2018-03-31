8f59c59fb3ff254d628e87bc8f538c395644a318
Daniel Mircea <daniel@viseztrance.com>
2018-04-01 00:41:47 +0100

    Update etags support

--- config/tags-setup.el
+++ config/tags-setup.el
@@ -1,19 +1,25 @@
+(require-package 'etags-select)
+
 (defun create-tags (&rest extensions)
   (let* ((root
-	  (substring 
-	   (shell-command-to-string "git rev-parse --show-toplevel")
-	   0 -1))
-	 (arguments (substring
-		     (mapconcat 'identity
-				(maplist #'(lambda (extension)
-						      (format "-o -name '*.%s'" (first extension)))
-						  extensions) " ")
-		     3))
-	 (command (format "cd %s && find . %s | egrep -v '#|~' | xargs etags -f TAGS" root arguments)))
+          (substring
+           (shell-command-to-string "git rev-parse --show-toplevel")
+           0 -1))
+         (arguments (substring
+                     (mapconcat 'identity
+                                (cl-maplist #'(lambda (extension)
+                                                (format "-o -name '*.%s'" (cl-first extension)))
+                                            extensions) " ")
+                     3))
+         (command (format "cd %s && find . %s | egrep -v '#|~' | xargs etags -f TAGS" root arguments)))
     (shell-command command)))
 
 (global-set-key [f8] (lambda ()
-		       (interactive)
-		       (create-tags "rb" "erb" "js" "php" "el")))
+                       (interactive)
+                       (create-tags "rb" "erb" "js" "php" "el")))
+
+(setq etags-select-go-if-unambiguous t)
+
+(global-set-key (kbd "M-.") 'etags-select-find-tag-at-point)
 
 (provide 'tags-setup)
