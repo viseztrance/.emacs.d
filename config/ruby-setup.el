7dffdbf4b01ded2910910831b8e2ad14c61b8cd1
Daniel Mircea <daniel@viseztrance.com>
2019-09-05 20:58:52 +0100

    Remove no longer used code

--- config/ruby-setup.el
+++ config/ruby-setup.el
@@ -1,10 +1,6 @@
 (require-package 'ruby-tools)
-(require-package 'rvm)
 
 (require 'ruby-tools)
-
-(require 'rvm)
-(rvm-use-default)
 
 (associate-files 'ruby ".rake" ".thor" "Gemfile" "Rakefile" "Capfile" ".json.jbuilder")
 
--- config/selection-setup.el
+++
@@ -1,6 +0,0 @@
-(require-package 'expand-region)
-
-(global-set-key (kbd "M-=") 'er/expand-region)
-(global-set-key (kbd "M-SPC") 'er/contract-region)
-
-(provide 'selection-setup)
