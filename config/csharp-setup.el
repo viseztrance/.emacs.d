bf20addea4422ca9a0bd812cda775514cc73d4d7
Daniel Mircea <daniel@viseztrance.com>
2019-09-05 20:59:36 +0100

    Fix csharp mode being loaded for css files

--- config/csharp-setup.el
+++ config/csharp-setup.el
@@ -6,6 +6,6 @@
             (setq-local omnisharp-server-executable-path "~/Software/omnisharp/omnisharp/start.sh")
             (setq-local tab-width 4)
             (setq-local c-basic-offset 4)
+            (c-set-offset 'arglist-cont '(c-lineup-arglist-operators 0))
             (omnisharp-mode)))
-
 (provide 'csharp-setup)
