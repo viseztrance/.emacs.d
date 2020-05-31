9ef9711c64ad0c41cd1ebac2385a697f095aaa98
Daniel Mircea <daniel@viseztrance.com>
2020-05-31 23:18:45 +0100

    Add autocomplete to csharp

--- config/csharp-setup.el
+++ config/csharp-setup.el
@@ -1,9 +1,12 @@
 (require-package 'omnisharp)
 (associate-files 'csharp ".cs")
 
+(eval-after-load
+    'company
+  '(add-to-list 'company-backends #'company-omnisharp))
+
 (add-hook 'csharp-mode-hook
           (lambda ()
-            (setq-local omnisharp-server-executable-path "~/Software/omnisharp/omnisharp/start.sh")
             (setq-local tab-width 4)
             (setq-local c-basic-offset 4)
             (c-set-offset 'arglist-cont '(c-lineup-arglist-operators 0))
