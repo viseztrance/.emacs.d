64913d7c68ac10c9faaacfdb6b0ddf38fb16319a
Daniel Mircea <daniel@viseztrance.com>
2021-09-06 11:54:20 +0300

    Remove marmalade repo source

--- config/package-management-setup.el
+++ config/package-management-setup.el
@@ -6,7 +6,6 @@
       (package-refresh-contents)
       (package-install name))))
 
-(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 
 (package-initialize)
