b0afd0982ab6ed7a7d584144f4a091261559988b
Daniel Mircea <daniel@viseztrance.com>
2022-03-12 00:54:05 +0200

    Allow magit to read git user and email from ENV variables

--- config/git-setup.el
+++ config/git-setup.el
@@ -7,4 +7,11 @@
 (set-face-attribute 'magit-section-highlight nil
                     :background "#444")
 
+(require-package 'exec-path-from-shell)
+(require 'exec-path-from-shell)
+(exec-path-from-shell-initialize)
+
+(exec-path-from-shell-copy-env "EMAIL")
+(exec-path-from-shell-copy-env "GIT_AUTHOR_NAME")
+
 (provide 'git-setup)
